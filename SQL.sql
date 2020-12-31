f1115700caaf662a806fee416d8b943332d24193
AlessandroPoloni <75130423+AlessandroPoloni@users.noreply.github.com>
2020-12-31 16:06:00 +0100

    Add files via upload

---
+++ SQL.sql
@@ -0,0 +1,16 @@
+create table utente(
+	username varchar(20) primary key,
+	password varchar(30) not null,
+	email varchar(320) not null,
+	dataIscrizione date not null,
+	isAdmin bit default b'0' not null,
+	isDeleted bit default b'0' not null
+);
+
+create table amicizia(
+	utente1 varchar(20),
+	utente2 varchar(20),
+	primary key(utente1, utente2),
+	foreign key(utente1) references utente(username) on update no action on delete no action,
+	foreign key(utente2) references utente(username) on update no action on delete no action
+);
